e0d337e1e4ff6ff8775ee99e7ab8cab4167174a4
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-20 21:00:06 +0530

    Add home http routes

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("App listening on", process.env.PORT);
+});
