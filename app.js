25d9fbf0f95fc511e2510736193a512a8b231f7e
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-20 21:05:44 +0530

    Add users and messages data

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const express = require("express");
 
 const app = express();
 
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
 app.get("/", (req, res) => {
   return res.send("Received a GET HTTP method");
 });
