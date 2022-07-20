5b1bf6aa755e43538f7e9df5ce47c0e2369f7765
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-20 21:01:18 +0530

    Add users routes with restful parameters

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ app.delete("/", (req, res) => {
   return res.send("Received a DELETE HTTP method");
 });
 
+app.get("/users", (req, res) => {
+  return res.send("GET HTTP method on user resource");
+});
+
+app.post("/users", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+app.put("/users/:userId", (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
+
 app.listen(process.env.PORT, () => {
   console.log("App listening on", process.env.PORT);
 });
